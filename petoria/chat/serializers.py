b8ad3539f576d0bfb5ba788cd19e2f8a731d897b
arshiasamimi <samimarsh44@gmail.com>
2025-11-19 21:39:42 +0330

    updating chat serializers

--- petoria/chat/serializers.py
+++ petoria/chat/serializers.py
@@ -1,68 +1,54 @@
-from users.models import User
-from users.serializers import BasicUserSerializer, BasicWithPFPUserSerializer
-from .models import Message, Chat
-
 from rest_framework import serializers
-from rest_framework.serializers import ModelSerializer
+from users.models import User
+from .models import Chat, Message, ChatParticipant
 
-
-class GetOrCreateChatSerializer(serializers.Serializer):
-    user_id = serializers.IntegerField()
-
-
-
-class InChatMessageSerializer(ModelSerializer):
-    sender = BasicWithPFPUserSerializer(read_only=True)
+class MessageSerializer(serializers.ModelSerializer):
+    sender_name = serializers.CharField(source='sender.username', read_only=True)
+    
     class Meta:
         model = Message
-        fields = ['id', 'sender', 'body', 'timestamp']
+        fields = ['id', 'sender', 'sender_name', 'content', 'timestamp', 'is_read']
 
-
-class ChatListMessageSerializer(ModelSerializer):
-    sender = BasicUserSerializer(read_only=True)
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Message
-        fields = ['id', 'sender', 'body', 'timestamp']
+        model = User
+        fields = ['id', 'username']
 
-
-
-class ChatListSerializer(ModelSerializer):
-    members = serializers.SerializerMethodField()
+class ChatSerializer(serializers.ModelSerializer):
+    other_participant = serializers.SerializerMethodField()
     last_message = serializers.SerializerMethodField()
     unread_count = serializers.SerializerMethodField()
-
+    last_message_time = serializers.DateTimeField(read_only=True)
+    
     class Meta:
         model = Chat
-        fields = [
-            'id',
-            'members',
-            'last_message',
-            'unread_count',
-        ]
-
-    def get_members(self, obj):
-        request = self.context.get('request')
-        if not request:
-            return []
-        
-        current_user_id = request.user.id
-
-        return [
-            BasicWithPFPUserSerializer(user, context=self.context).data
-            for user in obj.members.exclude(id=current_user_id)
-        ]
-
+        fields = ['id', 'other_participant', 'last_message', 'created_at', 'unread_count', 'last_message_time']
+    
+    def get_other_participant(self, obj):
+        current_user = self.context['request'].user
+        other_user = obj.participants.exclude(id=current_user.id).first()
+        return UserSerializer(other_user).data if other_user else None
+    
     def get_last_message(self, obj):
-        last_msg = obj.messages.last()
-        if last_msg:
-            return ChatListMessageSerializer(last_msg, context=self.context).data
-        return None
-
+        last_msg = obj.messages.first()
+        return MessageSerializer(last_msg).data if last_msg else None
+    
     def get_unread_count(self, obj):
-        request = self.context.get('request')
-        if not request:
+        try:
+            participant = obj.participants_info.get(user=self.context['request'].user)
+            return participant.unread_count
+        except ChatParticipant.DoesNotExist:
             return 0
-        
-        return obj.messages.filter(is_read=False)\
-                           .exclude(sender=request.user)\
-                           .count()
+
+class CreateChatSerializer(serializers.Serializer):
+    other_user_id = serializers.IntegerField()
+    message = serializers.CharField(max_length=5000)
+    
+    def validate_other_user_id(self, value):
+        if value == self.context['request'].user.id:
+            raise serializers.ValidationError("Cannot chat with yourself")
+        try:
+            User.objects.get(id=value)
+        except User.DoesNotExist:
+            raise serializers.ValidationError("User not found")
+        return value
