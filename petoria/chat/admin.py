99863d5cb31302a702a53c498c186f01e81cc35c
arshiasamimi <samimarsh44@gmail.com>
2025-11-19 21:53:19 +0330

    minor update

--- petoria/chat/admin.py
+++ petoria/chat/admin.py
@@ -1,12 +1,2 @@
 from django.contrib import admin
-from .models import Chat, Message
 # Register your models here.
-
-@admin.register(Chat)
-class ChatAdmin(admin.ModelAdmin):
-    list_display = ['id', 'is_private', 'created_at']
-    filter_horizontal = ['members']   # clean multi-select UI
-    
-@admin.register(Message)
-class MessageAdmin(admin.ModelAdmin):
-    pass
