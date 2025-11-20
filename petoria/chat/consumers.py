88aaa4d164735223c0937a935a683ed353bc2933
arshiasamimi <samimarsh44@gmail.com>
2025-11-20 11:51:19 +0330

    minor change

--- petoria/chat/consumers.py
+++ petoria/chat/consumers.py
@@ -1,7 +1,6 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 from channels.db import database_sync_to_async
-from django.contrib.auth import get_user_model
 from django.core.exceptions import ObjectDoesNotExist
 
 from .models import Chat, Message, ChatParticipant
