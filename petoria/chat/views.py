d3594238d3cd6790e8150d88ef7330f5fe1da299
arshiasamimi <samimarsh44@gmail.com>
2025-11-21 18:08:28 +0330

    add ChatWithUserAPIView

--- petoria/chat/views.py
+++ petoria/chat/views.py
@@ -1,11 +1,16 @@
 from rest_framework import generics, permissions
 from django.db.models import OuterRef, Subquery
-from .models import Chat, Message, ChatParticipant
+from .models import Chat, Message
 from .serializers import ChatSerializer, MessageSerializer
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
+from users.models import User
 
 # ------------------------------------------------------------
 # ChatListView
-# Returns the list of chats for the authenticated user.
+# Returns the list of chats for the authenticated user
 # ------------------------------------------------------------
 class ChatListView(generics.ListAPIView):
     serializer_class = ChatSerializer
@@ -34,7 +39,7 @@ class ChatListView(generics.ListAPIView):
 
 # ------------------------------------------------------------
 # ChatMessagesListView
-# Returns paginated messages for a chat.
+# Returns paginated messages for a chat
 # ------------------------------------------------------------
 class ChatMessagesListView(generics.ListAPIView):
     serializer_class = MessageSerializer
@@ -47,3 +52,42 @@ class ChatMessagesListView(generics.ListAPIView):
             .select_related("sender")
             .order_by("-created_at")  # newest first (pagination friendly)
         )
+
+# ------------------------------------------------------------
+# ChatWithUserAPIView
+# Returns the chat id given user ids if exists
+# else returns none
+# ------------------------------------------------------------
+class ChatWithUserAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, *args, **kwargs):
+        other_user_id = request.query_params.get("user_id")
+
+        if not other_user_id:
+            return Response(
+                {"error": "missing_user_id"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        if str(other_user_id) == str(request.user.id):
+            return Response(
+                {"error": "cannot_chat_with_self"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        # validate other user
+        try:
+            other_user = User.objects.get(id=other_user_id)
+        except User.DoesNotExist:
+            return Response(
+                {"error": "user_not_found"},
+                status=status.HTTP_404_NOT_FOUND
+            )
+
+        # find chat if exists
+        chat = Chat.get_chat_between(request.user, other_user)
+
+        return Response({
+            "chat_id": chat.id if chat else None
+        })
