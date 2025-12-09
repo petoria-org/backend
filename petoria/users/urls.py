5684a52d3d3851f26fae36671a23fdb3e7ab6e89
arshiasamimi <samimarsh44@gmail.com>
2025-12-09 21:41:44 +0330

    removing obtain jwt to enforce login api

--- petoria/users/urls.py
+++ petoria/users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
+from rest_framework_simplejwt.views import TokenRefreshView, TokenVerifyView
 
 from .views import SignupView, LoginView, RequestOTPView, VerifyOTPView, ResetPasswordView
 
@@ -9,7 +9,6 @@ urlpatterns = [
     path("otp/request/", RequestOTPView.as_view(), name="request-otp"),
     path("otp/verify/", VerifyOTPView.as_view(), name="verify-otp"),
     path("password/reset/", ResetPasswordView.as_view(), name="reset-password"),
-    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
 ]
