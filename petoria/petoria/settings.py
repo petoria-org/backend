b8bfb9aebaed64085bcb61129974893e20d1ae61
asabayatnaderi <asabayatnaderi2005@gmail.com>
2025-12-17 15:07:32 +0330

    adding cors header

--- petoria/petoria/settings.py
+++ petoria/petoria/settings.py
@@ -40,6 +40,7 @@ ALLOWED_HOSTS = []
 #   and so to be used in automations (and maybe other locations as well)
 
 THIRD_PARTY_APPS = [
+    "corsheaders",
     "rest_framework",
     "django_extensions",
     "channels",
@@ -72,6 +73,7 @@ INSTALLED_APPS = (
 )
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -214,3 +216,10 @@ SIMPLE_JWT = {
 # EMAIL_HOST_USER = "youremail@gmail.com"
 # EMAIL_HOST_PASSWORD = "12345"
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+]
